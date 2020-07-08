d3b23a52158df6136701e1a991dbc124dfa515b4
florian mutel <florian.mutel@gmail.com>
2020-07-08 14:15:54 +0200

    Add return value for specified exception type

--- retry/api.py
+++ retry/api.py
@@ -4,19 +4,20 @@ import time
 
 from functools import partial
 
-from .compat import decorator
+from retry.compat import decorator
 
 
 logging_logger = logging.getLogger(__name__)
 
 
-def __retry_internal(f, exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1, jitter=0,
+def __retry_internal(f, exceptions=Exception, default_return=(None, None), tries=-1, delay=0, max_delay=None, backoff=1, jitter=0,
                      logger=logging_logger):
     """
     Executes a function and retries it if it failed.
 
     :param f: the function to execute.
     :param exceptions: an exception or a tuple of exceptions to catch. default: Exception.
+    :param default_return: tuple (Exception, default return value) for this exception after n tries instead of raise
     :param tries: the maximum number of attempts. default: -1 (infinite).
     :param delay: initial delay between attempts. default: 0.
     :param max_delay: the maximum value of delay. default: None (no limit).
@@ -27,6 +28,7 @@ def __retry_internal(f, exceptions=Exception, tries=-1, delay=0, max_delay=None,
                    default: retry.logging_logger. if None, logging is disabled.
     :returns: the result of the f function.
     """
+    special_exception, return_value = default_return
     _tries, _delay = tries, delay
     while _tries:
         try:
@@ -34,7 +36,10 @@ def __retry_internal(f, exceptions=Exception, tries=-1, delay=0, max_delay=None,
         except exceptions as e:
             _tries -= 1
             if not _tries:
-                raise
+                if type(e) is special_exception:
+                    return return_value
+                else:
+                    raise
 
             if logger is not None:
                 logger.warning('%s, retrying in %s seconds...', e, _delay)
@@ -51,11 +56,12 @@ def __retry_internal(f, exceptions=Exception, tries=-1, delay=0, max_delay=None,
                 _delay = min(_delay, max_delay)
 
 
-def retry(exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1, jitter=0, logger=logging_logger):
+def retry(exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1, jitter=0, logger=logging_logger, default_return=(None, None)):
     """Returns a retry decorator.
 
     :param exceptions: an exception or a tuple of exceptions to catch. default: Exception.
     :param tries: the maximum number of attempts. default: -1 (infinite).
+    :param default_return: tuple (Exception, default return value) for this exception after n tries instead of raise
     :param delay: initial delay between attempts. default: 0.
     :param max_delay: the maximum value of delay. default: None (no limit).
     :param backoff: multiplier applied to delay between attempts. default: 1 (no backoff).
@@ -70,13 +76,13 @@ def retry(exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1, ji
     def retry_decorator(f, *fargs, **fkwargs):
         args = fargs if fargs else list()
         kwargs = fkwargs if fkwargs else dict()
-        return __retry_internal(partial(f, *args, **kwargs), exceptions, tries, delay, max_delay, backoff, jitter,
+        return __retry_internal(partial(f, *args, **kwargs), exceptions, default_return, tries, delay, max_delay, backoff, jitter,
                                 logger)
 
     return retry_decorator
 
 
-def retry_call(f, fargs=None, fkwargs=None, exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1,
+def retry_call(f, fargs=None, fkwargs=None, exceptions=Exception, default_return=(None, None), tries=-1, delay=0, max_delay=None, backoff=1,
                jitter=0,
                logger=logging_logger):
     """
@@ -86,6 +92,7 @@ def retry_call(f, fargs=None, fkwargs=None, exceptions=Exception, tries=-1, dela
     :param fargs: the positional arguments of the function to execute.
     :param fkwargs: the named arguments of the function to execute.
     :param exceptions: an exception or a tuple of exceptions to catch. default: Exception.
+    :param default_return: tuple (Exception, default return value) for this exception after n tries instead of raise
     :param tries: the maximum number of attempts. default: -1 (infinite).
     :param delay: initial delay between attempts. default: 0.
     :param max_delay: the maximum value of delay. default: None (no limit).
@@ -98,4 +105,4 @@ def retry_call(f, fargs=None, fkwargs=None, exceptions=Exception, tries=-1, dela
     """
     args = fargs if fargs else list()
     kwargs = fkwargs if fkwargs else dict()
-    return __retry_internal(partial(f, *args, **kwargs), exceptions, tries, delay, max_delay, backoff, jitter, logger)
+    return __retry_internal(partial(f, *args, **kwargs), exceptions, default_return, tries, delay, max_delay, backoff, jitter, logger)
